27e37fa5eaf444bfe2d3b003a11ef1eea013f139
AntonMakarkin <amakarkin27@gmail.com>
2021-01-21 13:58:36 +0300

    Made the lists

--- src/Stats.js
+++ src/Stats.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './Stats.css';
+import axios from "axios";
+import StatsRow from './StatsRow';
+
+const TOKEN = 'c0411o748v6qfu3ufb30';
+const BASE_URL = 'https://finnhub.io/api/v1/quote';
 
 function Stats() {
+
+    const [stockData, setstockData] = useState([]);
+
+    const getStockData = (stock) => {
+        return axios
+          .get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`)
+          .catch((error) => {
+              console.error("Error", error.message);
+          });
+
+    };
+
+    useEffect(() => {
+        let tempStocksData = [];
+        const stocksList = ["AAPL", "MSFT", "TSLA", "FB", "UBER", "DIS", "SBUX"];
+        let promises = [];
+        stocksList.map((stock) => {
+            promises.push(
+                getStockData(stock)
+                .then((res) => {
+                    tempStocksData.push({
+                        name: stock,
+                        ...res.data
+                    })
+                })
+            )
+        });
+
+        Promise.all(promises).then(() => {
+            setstockData(tempStocksData);
+        })
+    }, []);
+
     return (
         <div className="stats">
-            
+            <div className="stats__container">
+                <div className="stats__header">
+                    <p>Stocks</p>
+                </div>
+                <div className="stats__content">
+                    <div className="stats__rows">
+                   
+                    </div>
+                </div>
+                <div className="stats__header">
+                    <p>Lists</p>
+                </div>
+                <div className="stats__content">
+                    <div className="stats__rows">
+                        {stockData.map((stock) => (
+                            <StatsRow
+                                key={stock.name}
+                                name={stock.name}
+                                openPrice={stock.o}
+                                price={stock.c}/>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
